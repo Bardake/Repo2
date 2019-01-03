8cb02132f85b6d497e83b198d4d27b78b91e7f62
monica <guorongzhang@gmail.com>
2019-01-03 21:32:16 +0800

    把工程项目添加到git

---
+++ git_test_project/src/com/itheima/git/Demo.java
@@ -0,0 +1,17 @@
+package com.itheima.git;
+
+/**
+ * @Author: Chen_Jie
+ * @Date: 2019/1/3 21:29
+ * @Desc:
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+}
+
